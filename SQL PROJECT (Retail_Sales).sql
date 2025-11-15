32229c36d536962b84d154a2714751901a48e26b
SUSHMITA SAHOO <23sushsahoo.sushmita@gmail.com>
2025-11-15 20:01:42 +0530

    Add files via upload

--- SQL PROJECT (Retail_Sales).sql
+++ SQL PROJECT (Retail_Sales).sql
@@ -65,17 +65,29 @@ WHERE transactions_id IS NULL
 	total_sale IS NULL;
 
 
-DELETE FROM retail_sales
-WHERE 
-    sale_date IS NULL
-	OR sale_time IS NULL
-	OR customer_id IS NULL
-	OR gender IS NULL
-	OR age IS NULL 
-	OR category IS NULL 
-	OR quantity IS NULL
-	OR price_per_unit IS NULL 
-	OR cogs IS NULL;
+----to delect the record that have null value
+DELETE  FROM retail_sales
+WHERE transactions_id IS NULL 
+	OR
+	sale_date  IS NULL 
+	OR
+	sale_time IS NULL
+    OR
+	customer_id IS NULL
+	OR
+	gender IS NULL
+	OR
+	age IS NULL
+	OR
+    category  IS NULL
+	OR
+    quantity IS NULL
+	OR
+	price_per_unit IS NULL
+	OR
+     cogs IS NULL
+	OR
+	total_sale IS NULL;
 
 
 ----DATA EXPLORATION
@@ -242,128 +254,3 @@ GROUP BY shift
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-----to delect the record that have null value
-DELETE  FROM retail_sales
-WHERE transactions_id IS NULL 
-	OR
-	sale_date  IS NULL 
-	OR
-	sale_time IS NULL
-    OR
-	customer_id IS NULL
-	OR
-	gender IS NULL
-	OR
-	age IS NULL
-	OR
-    category  IS NULL
-	OR
-    quantity IS NULL
-	OR
-	price_per_unit IS NULL
-	OR
-     cogs IS NULL
-	OR
-	total_sale IS NULL;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
